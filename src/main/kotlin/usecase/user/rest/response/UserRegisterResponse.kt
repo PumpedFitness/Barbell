9a478b49601c0faeebe82a07c823dd48b0a97748
Timothy Spriegade <timothyspriegade@outlook.de>
2025-04-25 23:07:10 +0200

    register user works now

--- src/main/kotlin/usecase/user/rest/response/UserRegisterResponse.kt
+++ src/main/kotlin/usecase/user/rest/response/UserRegisterResponse.kt
@@ -1,5 +1,13 @@
 package ord.pumped.usecase.user.rest.response
 
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UserRegisterResponse(
+    val id: String,
     val username: String,
+    val email: String,
+    val createdAt: Instant,
+    val updatedAt: Instant
 )
