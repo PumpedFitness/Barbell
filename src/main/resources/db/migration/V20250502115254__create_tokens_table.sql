42965f94f31015f0e49ef1d951cd6da1de0d5c3c
devin <devin.fritz@nordakademie.de>
2025-05-02 11:53:53 +0200

    Add create token migrations

--- src/main/resources/db/migration/V20250502115254__create_tokens_table.sql
+++ src/main/resources/db/migration/V20250502115254__create_tokens_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE barbell_jwt_tokens(
-    id BINARY(16) PRIMARY KEY NOT NULL,
+CREATE TABLE tokens(
+    token_id BINARY(16) PRIMARY KEY NOT NULL,
     is_blacklisted BOOL default false NOT NULL
 )
