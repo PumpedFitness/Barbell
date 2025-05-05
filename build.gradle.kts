07c4a5a69a9fd9deb6d9408b0852bce2f3e0b6dc
Timothy Spriegade <timothy.spriegade@otto.de>
2025-05-05 10:37:01 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,9 +103,3 @@ dependencies {
     testImplementation("com.h2database:h2:${h2_version}")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
-
-sourceSets {
-    test {
-        resources.srcDir("ci/env")
-    }
-}
