477d8a47787b809760e38f1b7b0f13c30f271ce8
devin <devin.fritz@nordakademie.de>
2025-05-05 21:11:24 +0200

    Add RabbitMQ demo config

--- src/main/kotlin/configuration/RabbitMQ.kt
+++ src/main/kotlin/configuration/RabbitMQ.kt
@@ -1,13 +1,14 @@
 package ord.pumped.configuration
 
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.RabbitMQ
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import ord.pumped.io.env.EnvVariables
+import ord.pumped.io.env.env
 
 fun Application.configureRabbitMQ() {
-    log.info("Connecting to RabbitMQ")
     install(RabbitMQ) {
-        uri = "amqp://${secrets[EnvVariables.BB_RABBITMQ_USER]}:${secrets[EnvVariables.BB_RABBITMQ_PASSWORD]}@${secrets[EnvVariables.BB_RABBITMQ_HOST]}:${secrets[EnvVariables.BB_RABBITMQ_PORT]}"
+        uri = "amqp://${env[EnvVariables.BB_RABBITMQ_USER]}:${env[EnvVariables.BB_RABBITMQ_PASSWORD]}@${env[EnvVariables.BB_RABBITMQ_HOST]}:${env[EnvVariables.BB_RABBITMQ_PORT]}"
         defaultConnectionName = "default-connection"
         dispatcherThreadPollSize = 4
         tlsEnabled = false
