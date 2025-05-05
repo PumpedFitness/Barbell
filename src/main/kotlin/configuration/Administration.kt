4748f680eface2b78d0e16878d00f5905d4ba58a
devin <devin.fritz@nordakademie.de>
2025-05-05 12:53:17 +0200

    Add database monitoring

--- src/main/kotlin/configuration/Administration.kt
+++ src/main/kotlin/configuration/Administration.kt
@@ -1,12 +1,18 @@
 package ord.pumped.configuration
 
-import io.github.flaxoos.ktor.server.plugins.ratelimiter.RateLimiting
-import io.github.flaxoos.ktor.server.plugins.ratelimiter.implementations.TokenBucket
+import io.github.flaxoos.ktor.server.plugins.ratelimiter.*
+import io.github.flaxoos.ktor.server.plugins.ratelimiter.implementations.*
 import io.github.flaxoos.ktor.server.plugins.taskscheduling.TaskScheduling
 import io.github.flaxoos.ktor.server.plugins.taskscheduling.managers.lock.redis.redis
 import io.ktor.server.application.*
+import io.ktor.server.cio.CIO
+import io.ktor.server.cio.backend.httpServer
+import io.ktor.server.engine.EmbeddedServer
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.routing.*
 import ord.pumped.io.env.EnvVariables
+import ord.pumped.io.env.env
+import kotlin.system.exitProcess
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureAdministration() {
@@ -24,8 +30,8 @@ fun Application.configureAdministration() {
     install(TaskScheduling) {
         redis {
             connectionPoolInitialSize = 1
-            host = secrets[EnvVariables.BB_REDIS_HOST]
-            port = secrets[EnvVariables.BB_REDIS_PORT].toInt()
+            host = env[EnvVariables.BB_REDIS_HOST]
+            port = env[EnvVariables.BB_REDIS_PORT].toInt()
             connectionAcquisitionTimeoutMs = 1_000
             lockExpirationMs = 60_000
         }
