7d903267802f8071b4f81b577ec7a9781a07a1ba
Timothy Spriegade <timothyspriegade@outlook.de>
2025-05-10 19:25:35 +0200

    debug

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -14,6 +14,11 @@ fun main() {
 
 fun Application.module(testing: Boolean = false) {
     println("Starting application")
+
+    print(System.getenv("BB_SECRET_ADAPTER"))
+    print(System.getenv("BB_VAULT_HOST"))
+    print(System.getenv("BB_VAULT_TOKEN"))
+    print(System.getenv("BB_VAULT_PATH"))
     println("configuring Secrets")
     configureSecrets()
 
