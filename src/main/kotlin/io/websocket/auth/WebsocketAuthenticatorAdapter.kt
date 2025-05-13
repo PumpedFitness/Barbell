4c999e3a560cd3912cfbce4a9f4f2c223aa248dc
devin <devin.fritz@nordakademie.de>
2025-05-13 09:46:34 +0200

    Fixed issue causing uncaught exception

--- src/main/kotlin/io/websocket/auth/WebsocketAuthenticatorAdapter.kt
+++ src/main/kotlin/io/websocket/auth/WebsocketAuthenticatorAdapter.kt
@@ -1,6 +1,7 @@
 package ord.pumped.io.websocket.auth
 
 import io.ktor.server.application.*
+import ord.pumped.common.exceptions.UnauthorizedException
 import ord.pumped.configuration.userID
 import ord.pumped.usecase.user.domain.mapper.UserModelMapper
 import ord.pumped.usecase.user.domain.model.User
@@ -14,7 +15,11 @@ class WebsocketAuthenticatorAdapter: IWebsocketAuthenticator, KoinComponent {
     private val userMapper by inject<UserModelMapper>()
 
     override fun authenticate(call: ApplicationCall): User? {
-        val userID = call.userID()
+        val userID = try {
+            call.userID()
+        } catch (e: UnauthorizedException) {
+            return null
+        }
 
         val userDTO = userRepository.findByID(userID) ?: return null
         return userMapper.toDomain(userDTO)
